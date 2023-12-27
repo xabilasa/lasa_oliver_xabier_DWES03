52a5d9b8bea6b6dad75096b20a4f09a7a6048492
xabilasa <105173750+xabilasa@users.noreply.github.com>
2023-12-27 22:25:37 +0100

    Add files via upload

---
+++ api/view/index.php
@@ -0,0 +1,9 @@
+<form method="post" action="controllers/userController.php">
+    <label for="username">Usuario:</label>
+    <input type="text" id="username" name="username">
+    <br>
+    <label for="password">Contraseña:</label>
+    <input type="password" id="password" name="password">
+    <br>
+    <button type="submit">Enviar</button>
+</form>
